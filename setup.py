59847d345d488da6197b334d7f5df200382c27aa
Forest Gregg <fgregg@uchicago.edu>
2018-02-19 21:01:55 -0600

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError :
 setup(
     name='parseratorvariable',
     url='https://github.com/datamade/parseratorvariables',
-    version='0.0.16',
+    version='0.0.17',
     description='Structured variable type for dedupe',
     packages=['parseratorvariable'],
     install_requires=['dedupe>=1.0.0', 'future'],
