da39e902c8a6bab78582ba4e61512a2c98f548d3
Forest Gregg <fgregg@uchicago.edu>
2018-05-18 15:44:03 -0500

    use conditional dependency in requirements

--- setup.py
+++ setup.py
@@ -3,21 +3,14 @@ try:
 except ImportError :
     raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
 
-requirements = ['dedupe>=1.0.0',
-                'future']
-
-try:
-    from functools import lru_cache
-except ImportError:
-    requirements += ['backports.functools_lru_cache']
-
-
 setup(
     name='parseratorvariable',
     url='https://github.com/datamade/parseratorvariables',
     version='0.0.17',
     description='Structured variable type for dedupe',
     packages=['parseratorvariable'],
-    install_requires=requirements,
+    install_requires=['dedupe>=1.0.0',
+                      'future',
+                      "backports.functools_lru_cache; python_version<'3.2'"],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
     )
