750155d918e6db268f8c691ba9c02b5e8b48f07a
Forest Gregg <fgregg@uchicago.edu>
2018-05-18 16:01:09 -0500

    accurate dpendencies

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     description='Structured variable type for dedupe',
     packages=['parseratorvariable'],
     install_requires=['dedupe>=1.0.0',
-                      'future',
-                      "backports.functools_lru_cache; python_version<'3.2'"],
+                      "backports.functools_lru_cache; python_version<'3.2'",
+                      'numpy',
+                      'probableparsing'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
     )
